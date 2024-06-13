4aae80b201c905eeec56bbae6ab54a665210f257
bwssomveer <121486500+bwssomveer@users.noreply.github.com>
2024-06-13 16:31:12 +0530

    first commit

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express();
 const cors = require("cors");
 // Your route handler
 app.use(cors());
-const userRoutes = require('../Buzzzz/src/User/Routes/RegisterUserRoutes');
-const CommonApiRoutes=require('../Buzzzz/src/User/Routes/CommonApiRounter');
-const ProfilePostRoutes=require('../Buzzzz/src/User/Routes/ProfilePostRouter');
-const PostLikeRoutes= require ('../Buzzzz/src/User/Routes/PostLikeRouter');
-const PostCommentRoutes = require ('../Buzzzz/src/User/Routes/PostCommentRouter');
-const BarRouter = require ('../Buzzzz/src/User/Routes/BarRouter');
-const CheckInRouter = require('../Buzzzz/src/User/Routes/CheckInRouter');
+const userRoutes = require('./src/User/Routes/RegisterUserRoutes');
+const CommonApiRoutes=require('./src/User/Routes/CommonApiRounter');
+const ProfilePostRoutes=require('./src/User/Routes/ProfilePostRouter');
+const PostLikeRoutes= require ('./src/User/Routes/PostLikeRouter');
+const PostCommentRoutes = require ('./src/User/Routes/PostCommentRouter');
+const BarRouter = require ('./src/User/Routes/BarRouter');
+const CheckInRouter = require('./src/User/Routes/CheckInRouter');
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/CommonApi',CommonApiRoutes);
@@ -20,6 +20,6 @@ app.use('/PostComment',PostCommentRoutes);
 app.use('/Bar',BarRouter);
 app.use('/CheckIn',CheckInRouter);
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(4006, () => {
+  console.log('Server is running on port 4006');
 });
