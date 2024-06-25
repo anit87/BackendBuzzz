c9e7211bf03b06b56e161a4119087fd78fe73123
bwssomveer <121486500+bwssomveer@users.noreply.github.com>
2024-06-25 12:25:25 +0530

    Update_Buzzz25

--- DataBase/Connection.js
+++ DataBase/Connection.js
@@ -1,35 +1,14 @@
 //const { json } = require('body-parser');
 const {createPool}= require ("mysql2");
+require("dotenv").config();
+
 const pool = createPool ({
     port:3306,
     host:'localhost',
     user:'root',
-    password:'',
-    database:'buzzzdb'
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 
 });
-// var pool = mysqlConnection.connect((err)=>
-// {
-//  if(err)
-//         {
-//             console.log('Error in Database Connnection :'+ JSON.stringify(err,undefined,2));
-//         }
-//         else
-//         {
-//             console.log('DataBase Connection Sucessfully')
-//         }
-// })
+
  module.exports=pool;
-
-// const {createPool} = require("mysql");
-
-// const pool = createPool ({
-//     port:3306,
-//     host:"localhost",
-//     user : "root",
-//     password:"",
-//     database : "level_X"
-
-// });
-
-// module.exports = pool;
